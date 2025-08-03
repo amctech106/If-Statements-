4774bb0bf7286e24bfc52022909bd286459cdcb5
amctech106 <121874402+amctech106@users.noreply.github.com>
2025-08-03 11:14:24 +0500

    If Conditions

---
+++ If Conditions/script.js
@@ -0,0 +1,25 @@
+///---------If Conditions----------
+
+/*let hour = 20;
+
+if(hour >= 21){
+    console.log("Good Morning");
+} else if ( hour >= 22){
+    console.log ("God Evening");
+} else {
+    console.log("Good Night");
+}*/
+
+
+let time = 8;
+
+if( time <= 5){
+    console.log("Fajar Prayer");
+} else if ( time <= 6){
+    console.log("Zuhur Prayer");
+} else if ( time <= 7){
+    console.log( "As'r Prayer");
+} else{
+    console. log ("Magrib Prayer");
+}
+
